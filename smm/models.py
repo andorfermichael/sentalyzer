65342ccda545b029f0861dd4d0926161e691a665
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-05-09 10:43:08 +0200

    [bugfix] fix bug which causes error due to wrong encoding

--- smm/models.py
+++ smm/models.py
@@ -4,7 +4,7 @@ from bson import ObjectId
 import datetime
 import hashlib
 import pickle
-import StringIO
+from io import StringIO
 
 from smm import config
 from smm.classifier import labels
@@ -114,7 +114,7 @@ class TrainedClassifiers(mongoengine.Document):
     stats = mongoengine.DictField()
 
     def set_classifier(self, classifier):
-        s = StringIO.StringIO(pickle.dumps(classifier))
+        s = io.StringIO(pickle.dumps(classifier).encode('utf-8'))
         self.classifier.put(s)
 
     def get_classifier(self):
