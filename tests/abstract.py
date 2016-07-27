98f7e0ab4ae4edb861f21779e66c9f6ef4aa4f83
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-07-27 18:07:15 +0200

    [test] remove tests

--- tests/abstract.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-import mongoengine
-from smm.config import test_mongo_db
-from smm import models
-
-class TestCaseDB(TestCase):
-
-    def setUp(self):
-        models.connect(test_mongo_db)
